f11863ed54d92756e07c3af8739b157a1026e1b1
Sergey Tarasov <tarasov-mnm@yandex.ru>
2022-04-13 18:34:34 +0300

    initial commit

---
+++ app/src/main/java/com/sergio994350/stockmarketcheck/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.sergio994350.stockmarketcheck
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.sergio994350.stockmarketcheck.ui.theme.StockMarketAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockMarketAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
