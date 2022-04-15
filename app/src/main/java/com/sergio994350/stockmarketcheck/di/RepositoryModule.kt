f87578cc88edf07532ab1e792418584a8b6d360b
Sergey Tarasov <tarasov-mnm@yandex.ru>
2022-04-15 20:15:43 +0300

    1st stage - load listing, keep in cache, screen in dark theme

---
+++ app/src/main/java/com/sergio994350/stockmarketcheck/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.sergio994350.stockmarketcheck.di
+
+import com.sergio994350.stockmarketcheck.data.csv.CSVParser
+import com.sergio994350.stockmarketcheck.data.csv.CompanyListingsParser
+import com.sergio994350.stockmarketcheck.data.repository.StockRepositoryImpl
+import com.sergio994350.stockmarketcheck.domain.model.CompanyListing
+import com.sergio994350.stockmarketcheck.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
